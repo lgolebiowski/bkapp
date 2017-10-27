6c34f6969442f406dc127fbc39e9e6027564467a
Masujkule <lukasz.golebiowski@gmail.com>
2017-10-27 20:29:25 +1100

    add LoginForm and solved submit

---
+++ src/components/forms/LoginForm.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import { Form, Button } from 'semantic-ui-react';
+import Validator from 'validator'; 
+import InlineError from '../messages/InlineError';
+import PropTypes from 'prop-types';
+
+
+
+class LoginForm extends React.Component {
+
+state = {
+	data: {
+		email: "",
+		password: "",
+	},
+	loading: false,
+	errors: {}
+};
+
+	onChange = e => this.setState(
+			{ data: { ...this.state.data, [e.target.name]: e.target.value }
+		});
+
+	onSubmit = () => {
+		const errors = this.validate(this.state.data);
+		this.setState( {errors} );
+		if (Object.keys(errors).length===0) {
+			this.props.submit(this.state.data);
+		}
+	};
+
+	validate = ( data ) => {
+		const errors = {}
+		if(!data.password) errors.password = "Masujkule can't be blank!";
+		if (!Validator.isEmail(data.email)) errors.email = "Invalid Email"
+		return errors;
+	};
+
+render() {
+
+	const { data, errors } = this.state;
+
+	return (
+			<Form onSubmit = { this.onSubmit }>
+				<Form.Field>
+					<label htmlFor='email'>Email</label>
+					<input 
+						type='email' 
+						id='email' 
+						name='email'
+						placeholder='your email'
+						value= { data.email }
+						onChange = { this.onChange }
+						/>
+						{ errors.email && <InlineError text={errors.email} />}
+				</Form.Field>
+
+				<Form.Field>
+					<label htmlFor='password'>password</label>
+					<input 
+						type='password' 
+						id='password' 
+						name='password'
+						placeholder='your password'
+						value= { data.password }
+						onChange = { this.onChange }
+						/>
+						{ errors.password && <InlineError text={errors.password } />}
+
+				</Form.Field>
+				
+				<Button primary></Button>
+			</Form>
+		);
+	}	
+}
+
+LoginForm.propTypes = {
+	submit: PropTypes.func.isRequired
+};
+
+
+
+export default LoginForm;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,15 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import 'semantic-ui-css/semantic.min.css';
+import { CreateStore, ApplyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'react-thunk';
+
+// const store = createStore(
+// 		rootReducer,
+// 		applyMiddleware(thunk)
+// );
 
 
 ReactDOM.render(
